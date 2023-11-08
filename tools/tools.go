310ef8ca073845eccb79f885e4d36efa7b625991
luis <luisfelipemourapereira@protonmail.com>
2023-11-07 19:35:26 -0500

    [terraform-provider-wpengine] - init from scaffolding repo

---
+++ tools/tools.go
@@ -0,0 +1,11 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+// +build tools
+
+package tools
+
+import (
+	// document generation
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)
